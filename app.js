3ba57e4714d3cba562385281eb2fed866b00894d
AnjaliThawani <anjaliwebdeveloper24@gmail.com>
2024-04-17 12:59:33 +0530

    created header functional component

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = (
-  <div>
-    <h1 id="heading">Hello React</h1>
-    <a href="#">link</a>
-  </div>
-)
+const LogoComponent = () => <h1><span id="firstName">Anjali</span> Tech</h1>
 
-// React.createElement("h1", { id: "heading" }, "Hello React");
-// did minor change
-// console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const NavComponent = () => {
+  return (
+    <div className="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Career</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </div>
+  )
+}
+const Header = () => {
+  return (
+    <div className="container">
+      <LogoComponent />
+      <NavComponent />
+    </div>
+  )
+};
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-console.log(root, 'ttt')
-root.render(heading)
+root.render(<Header />)
