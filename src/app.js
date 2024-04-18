b3d42b53b99809237344f95183485af848597ce3
AnjaliThawani <anjaliwebdeveloper24@gmail.com>
2024-04-18 21:07:53 +0530

    fix a bug

--- src/app.js
+++ src/app.js
@@ -7,38 +7,37 @@ import { foodObject } from './utils/foodObject';
 import { isEmpty } from 'lodash'
 const AppLayoutComponent = () => {
   let [ListFoodItem, setListFoodItem] = useState(foodObject);
-  let [SearchFoodItem, setFoodItem] = useState(foodObject);
-  let List = false, Search = false;
+  let [SearchFoodItem, setFoodItem] = useState([]);
 
   const filterData = () => {
-    List = true;
     const filterFoodList = ListFoodItem.filter(item => item.rating > 3);
     setListFoodItem(filterFoodList)
   }
 
   const searchData = (name) => {
-    Search = true
-
     if (!isEmpty(name)) {
       const searchString = String(name).toLowerCase();
       const filteredData = ListFoodItem.filter(item => String(item.resName).toLowerCase().includes(searchString));
-      if (!isEmpty(filteredData)) {
-        setFoodItem(filteredData);
-      } else {
-
+      if (!isEmpty(filteredData)) setFoodItem(filteredData);
+      else {
         setFoodItem("Not_Found");
-
       }
-
     }
     else {
-      setFoodItem(null);
+      setFoodItem([]);
     }
   }
+
   return (
     <div className="app">
       <HeaderComponent applyFilter={filterData} />
-      <MainComponent applySearch={searchData} setFoodList={(SearchFoodItem != null) ? SearchFoodItem : ListFoodItem} />
+      <MainComponent
+        applySearch={searchData}
+        setFoodList={
+          (!isEmpty(SearchFoodItem)) ?
+            SearchFoodItem :
+            ListFoodItem
+        } />
       <FooterComponent />
     </div>
   )
